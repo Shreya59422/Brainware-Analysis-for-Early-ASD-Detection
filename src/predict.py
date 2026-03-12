038f7e32a4bdf75e387e3b69684b36ac53c04578
SHREYA REDDY S <shreyareddyrs22@gmail.com>
2026-03-12 07:27:44 +0530

    Update predict.py

--- src/predict.py
+++ src/predict.py
@@ -1,7 +1,10 @@
-Purpose
-This script demonstrates model deployment.
+import pandas as pd
+import joblib
 
-What it does
-1. loads trained model
-2. reads EEG features from Excel input
-3. predicts ASD risk
+model = joblib.load("asd_model.pkl")
+
+input_data = pd.read_excel("asd_eeg_input_template.xlsx")
+
+prediction = model.predict(input_data.values)
+
+print("ASD Prediction:", prediction)
