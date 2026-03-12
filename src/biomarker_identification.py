2ce5551ebc722ca3e1972fc3d768f8009cff875b
SHREYA REDDY S <shreyareddyrs22@gmail.com>
2026-03-12 07:27:15 +0530

    Update biomarker_identification.py

--- src/biomarker_identification.py
+++ src/biomarker_identification.py
@@ -1,7 +1,24 @@
-Purpose
-This script identifies EEG biomarkers influencing ASD prediction.
+import pandas as pd
+import shap
+import joblib
 
-What it does
-1. loads trained model
-2. uses SHAP explainability
-3. finds most important EEG features
+model = joblib.load("asd_model.pkl")
+
+data = pd.read_excel("data/Dataset.xlsx")
+
+features = [
+"delta_F_sx","delta_F_dx",
+"theta_F_sx","theta_F_dx",
+"low_alpha_F_sx","low_alpha_F_dx",
+"high_alpha_F_sx","high_alpha_F_dx",
+"beta_F_sx","beta_F_dx",
+"gamma_F_sx","gamma_F_dx"
+]
+
+X = data[features]
+
+explainer = shap.Explainer(model)
+
+shap_values = explainer(X)
+
+shap.summary_plot(shap_values, X)
